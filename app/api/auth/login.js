6a04582880983540afbe65838e6a8ef5e0260f59
Theo Otto <theorochaotto@gmail.com>
2025-04-27 12:00:24 -0300

    fix:login
    quase pronto

---
+++ app/api/auth/login.js
@@ -0,0 +1,27 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    var email = formData.get('email');
+    var senha = formData.get('senha');
+    // alert(email)
+
+    var ok = "";
+    
+    requisitar('POST', '/api/login', formData, 'formdata')
+        .then(result => {
+            if (result.erro) {
+                console.log(result);
+                console.error('Erro:', result.erro);
+                alert('Ocorreu um erro ao enviar os dados. Tente novamente.');
+				ok = false;
+            } else {
+                console.log(result);
+                if (result.dados.sucesso) {
+                    window.location = './';
+                } else {
+                    alert("Usuário inválido!");
+                }
+            }
+            // window.location.reload();
+        });
+});
