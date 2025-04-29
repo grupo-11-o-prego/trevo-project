6f3961c9859be0823343e459043332e759bf2806
Hector Vieira Saldivar <87508113+hector-vs@users.noreply.github.com>
2025-04-29 20:02:10 -0300

    feat: getPerfil

---
+++ app/models/PerfilModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+include_once __DIR__ . '/../../database/TimeStamp.php';
+include_once __DIR__ . '/Model.php';
+
+use Database\TimeStamp;
+use App\Models\Model;
+
+class PerfilModel extends Model {
+
+    public function getPerfil($user) {
+        try{
+            // Pega o Hash da senha a partir do e-mail digitado.
+            $stmt = $this->conn->prepare("SELECT * FROM usuarios_tb WHERE user_nome = :nome");
+            $stmt->bindParam(':nome', $user);
+
+            $stmt->execute();
+            $user = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+            if ($user) {
+                return ['sucesso' => true, 'result' => $user];
+            } else {
+                return ['sucesso' => false, 'result' => null];
+            }
+
+            return false;
+
+        } catch (\Exception $e) {
+            return ['sucesso' => false, "message" => 'Erro ao buscar usuario: ' . $e->getMessage()];
+        }
+    }
+    
+}
