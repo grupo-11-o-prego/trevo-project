a3b381e15add4a3ac194d4ef6702d151da2e5a1b
Gabriel moulin <gabrielpmoulin51@gmail.com>
2025-05-24 21:26:31 -0300

    feat: botão whatsapp dinamico com numro vendedor

--- app/api/tela/anuncio_detalhes_tela.js
+++ app/api/tela/anuncio_detalhes_tela.js
@@ -51,16 +51,42 @@ window.onload = async function () {
     titulo.textContent = post.anun_titulo;
     capa.appendChild(titulo);
 
-    // const whats = document.createElement('button');
-    // whats.className = 'flex items-center justify-center text-red-500 hover:text-red-600 border border-red-500 hover:border-red-600 py-2 px-3 rounded-md text-sm transition cursor-pointer bg-green-500 hover:scale-105'
-    // whats.innerHTML = `
-    //               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
-    //     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-    //   </svg>
-    // `
-    // capa.appendChild(whats);
-    
-    
+    const whats = document.createElement('button');
+    whats.className = 'flex items-center justify-center text-white hover:text-white border border-white hover:border-red-600 py-2 px-3 rounded-md text-sm transition cursor-pointer bg-green-500 hover:scale-105'
+    whats.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+        <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+      </svg>
+    `;
+    capa.appendChild(whats);    
+
+    whats.onclick = () => {
+      Swal.fire({
+        title: "Você quer entrar em contato via WhatsApp?",
+        text: "Você será redirecionado para o WhatsApp.",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#25D366",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim, abrir WhatsApp!",
+        cancelButtonText: "Cancelar",
+      }).then((res) => {
+        if (res.isConfirmed) {          
+          const numero = post.user_contato?.replace(/\D/g, '');
+          if (numero) {
+            const mensagem = "Olá! Vi seu anúncio e gostaria de conversar.";
+            const link = `https://wa.me/55${numero}?text=${mensagem}`;
+            window.open(link,'_blank');
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Erro',
+              text: 'Número de contato do vendedor não encontrado.',
+            });
+          }
+        }
+      });
+    };
 
 
     const conteudo = document.createElement('div');
