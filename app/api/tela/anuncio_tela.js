8c8d2f543073c001a394ec66bf432af62b90680c
Theo Otto <theorochaotto@gmail.com>
2025-05-30 19:53:34 -0300

    fix: Botao to denuncia

--- app/api/tela/anuncio_tela.js
+++ app/api/tela/anuncio_tela.js
@@ -1,9 +1,5 @@
 window.onload = async function () {
     if (document.getElementById('posts-container')) {
-        const response = await fetch('/trevo-project/public/api/perfil');
-        const data = response.dados.users;
-        console.log('admin', data.user_moderador);
-        
         await requisitar('GET', '/trevo-project/public/api/anuncio/listar')
             .then(result => {
                 console.log(result)
@@ -15,20 +11,6 @@ window.onload = async function () {
                     console.log(result.dados);
 
                     if (result.dados.sucesso && result.dados.result) {
-                        
-                        if (isModerador==1) {
-                            const denunciaButton = document.createElement('div');
-                            denunciaButton.href = '/trevo-project/public/denuncia';
-                            denunciaButton.className = 'w-10 h-10 rounded-full bg-[#D92323] text-white flex items-center justify-center hover:bg-[#a31919] transition-all ml-2';
-                            denunciaButton.innerHTML = `
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
-                                <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
-                                <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
-                              </svg>
-                            `;        
-                          }else{
-                            console.log('oi');
-                          }
                         result.dados.result.forEach(post => {
                             console.log(result)
 
