b8d5a06380bcd3d2372b8c45c03c64936fe77f82
Theo Otto <theorochaotto@gmail.com>
2025-06-01 21:58:03 -0300

    feat: Alterar Anuncio Estado

---
+++ app/api/auth/anuncio/alterarestado.js
@@ -0,0 +1,35 @@
+function handleSubmitEstado() {
+    const form = document.getElementById("alterarestado-form");
+    
+    const formData = new FormData(form);
+    var contato = formData.get('contato');
+    
+    var ok = "";
+    requisitar('POST', '/trevo-project/public/api/anuncio/alterarestado', formData, 'formdata')
+        .then(result => {
+            console.log(result)
+            if (result.erro) {
+                console.log('ERRO', result);
+                console.error('Erro:', result.erro);
+                alert('Ocorreu um erro ao enviar os dados. Tente a.');
+                ok = false;
+            } else {
+                console.log(result);
+                if (result.dados.sucesso) {
+                    Swal.fire({
+                        icon: "success",
+                        title: "Estado atualizado!",
+                        showConfirmButton: false,
+                        timer: 1500,
+                        timerProgressBar: true,
+                         willClose: () => {
+                           window.location.reload()
+                        }
+                      });
+                } else {
+                    alert(result.dados.message);
+                }
+            }
+            // window.location.reload();
+        });
+}
